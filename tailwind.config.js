36974dd81a9726d5100c2e8ab4b6a36fa16643b7
Nicolas2840f <ncastaneda840rmisena.edu.co>
2024-01-25 09:56:30 -0500

    Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/templates/**/*.html",
+    "./app/static/src/**/*.js", 
+    "./app/static/src/**/*.css" 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
