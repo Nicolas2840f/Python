21f1b580b82e6ef7f3beb7ae42dcdabf5a870137
Nicolas Castañeda Pardo <Nicolas2840f>
2024-02-12 09:58:33 -0500

    Commit

--- app/routes/pelicula_routes.py
+++ app/routes/pelicula_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from app.models.pelicula import Pelicula
+from app.models.genero import Genero
 from flask_login import login_user, logout_user, login_required
 from app import db, create_app
 from werkzeug.utils import secure_filename
@@ -19,6 +20,7 @@ def index():
 def add():
     from app import create_app  # Importa dentro de la función para evitar el ciclo de importación circular
     app = create_app()
+    
     if request.method == 'POST':
         nombre = request.form['nombrePelicula']
         descripcion = request.form['descripcionPelicula']
@@ -41,33 +43,38 @@ def add():
         db.session.commit()
         
         return redirect(url_for('pelicula.index'))
-    return render_template('peliculas/add.html')
+    generos = Genero.query.all()
+    return render_template('peliculas/add.html',generos = generos)
 
 
 @bp.route('/Pelicula/edit/<int:id>', methods=['GET', 'POST'])
 @login_required
 def edit(id):
-    # from app import create_app  # Importa dentro de la función para evitar el ciclo de importación circular
-    # app = create_app()
+    from app import create_app  # Importa dentro de la función para evitar el ciclo de importación circular
+    app = create_app()
     pelicula = Pelicula.query.get_or_404(id)
     
     if request.method == 'POST':
         pelicula.nombre = request.form['nombrePelicula']
         pelicula.descripcion = request.form['descripcionPelicula']
-        pelicula.imagen = request.form['imagenPelicula']
         pelicula.genero = request.form['generoPelicula']
+        imagen = request.files['imagenPelicula']
+        if imagen:
+            # Guardar la imagen en la carpeta de imágenes dentro de la carpeta 'static'
+            filename = secure_filename(imagen.filename)
+            imagen.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # Guardar la ruta relativa al archivo en la base de datos
+            pelicula.imagen =  filename
         
         db.session.commit()
         
         return redirect(url_for('pelicula.index'))
-
-    return render_template('peliculas/edit.html', pelicula=pelicula)
+    generos = Genero.query.all()
+    return render_template('peliculas/edit.html', pelicula=pelicula,generos = generos)
 
 @bp.route('/Pelicula/delete/<int:id>', methods=['GET', 'POST'])
 @login_required
 def delete(id):
-    from app import create_app  # Importa dentro de la función para evitar el ciclo de importación circular
-    app = create_app()
     pelicula = Pelicula.query.get_or_404(id)
     
     db.session.delete(pelicula)
